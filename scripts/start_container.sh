eb1240b6e00242497781e496a1af40fce6ba0aaa
Shoaib Bin Aziz <83201856+Shoaib-Neurochip@users.noreply.github.com>
2024-10-30 12:10:18 +0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull nishankkoul/simple-python-flask-app
+docker pull neurosba/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 8000:5000 nishankkoul/simple-python-flask-app
-
+docker run -d -p 8000:5000 neurosba/simple-python-flask-app
 
