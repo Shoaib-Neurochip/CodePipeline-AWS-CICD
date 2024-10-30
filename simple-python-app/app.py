15526e7361fe4d0c1f8862410f391f5ea3de2902
Shoaib Bin Aziz <83201856+Shoaib-Neurochip@users.noreply.github.com>
2024-10-30 13:36:32 +0600

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Ml ops sample test of CI/CD Pipeline.. ALIFA KHAN'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # Listen on all interfaces
